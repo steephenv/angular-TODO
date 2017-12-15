682cc5cfd80f59089abc9e57ad0e4292cb1746b4
steephen varghese <steephenvrs3@gmail.com>
2017-12-15 13:14:39 +0530

    dewfef

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  itemCount: number;
+  btnText: string = 'add an item';
+  goalText:string = 'enter the task';
+  goals = [];
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.itemCount=this.goals.length;
+
+  }
+  addItem()
+  {
+    this.goals.push(this.goalText);
+    this.goalText='';
+    this.itemCount=this.goals.length;
+  }
+  removeItem(i)
+  {
+    this.goals.splice(i,1);
+
+  }
+}
